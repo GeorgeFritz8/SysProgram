71e199283ab3736f1bde826b3d138efd8522c5be
GeorgeFritz8 <30572@ma-web.nl>
2021-11-05 11:07:55 +0100

    Debug Opdracht

---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DebuggingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[10];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point() { x = i, y = 0 };
+            }
+            string msg = null;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine($"from {i} to {i + 1} the distance is:");
+                int dist = CalcDist(points[i].x, points[i].y);
+                Console.WriteLine(dist);
+                if (dist == 1)
+                {
+                    msg = $"{i} and {i + 1} are next to eachother";
+                }
+            }
+
+            Console.WriteLine(msg.ToLower());
+            Console.ReadLine();
+        }
+
+        private static int CalcDist(int from, int to)
+        {
+            int result = from - to;
+
+            return result;
+        }
+
+        class Point
+        {
+            public int x, y;
+        }
+
+    }
+}
