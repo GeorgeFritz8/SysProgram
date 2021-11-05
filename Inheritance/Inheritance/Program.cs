7b2b82c17ca2a9aa5865715ac510aad2c257e839
GeorgeFritz8 <30572@ma-web.nl>
2021-11-05 10:08:01 +0100

    Updates

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Inheritance
+
+namespace SysProg
 {
     class Program
     {
--- Rectangle/Rectangle/Program.cs
+++ Rectangle/Rectangle/Program.cs
@@ -2,38 +2,44 @@
 
 namespace Rectangle
 {
+    class Program
+    {
+        static void Main(string[] args) {
+            Rectangle rectangle = new Rectangle(100, 100, 50, 60);
+            Rectangle square = new Rectangle(50, 50, 50, 50);
+            Rectangle squareMove = new Rectangle(50, 50, 50, 50);
+            Rectangle rectanglemove = new Rectangle(100, 100, 20, 30);
+        }
+    }
+
     class Rectangle
     {
-        public double width;
-        public double height;
+        private bool _isSquare;
+        private float x, y, width, height;
 
-        public Rectangle(double w, double h)
-        {
-            width = w;
-            height = h;
-        }
-
-        public double perimeter()
-        {
-            return 2 * (width + height);
-        }
-        public double area()
-        {
-            return width * height;
-        }
-        public bool isSquare()
-        {
-            if (width == height)
-            {
-                return true;
+        public Rectangle(float x, float y, float width, float height) {
+            if(width == height) {
+                _isSquare = true;
             }
-            else
-            {
-                return false;
+            Set(x, y, width, height);
+        }
+        public void MoveTo(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+        public bool isSquare() {
+            return _isSquare;
+        }
+        public void Set(float x, float y, float width, float height) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            if (_isSquare) {
+                Console.WriteLine($"Made new square at {x} {y} with {width} width, height {height}");
+            } else {
+                Console.WriteLine($"Made new rectangle at {x} {y} with {width} width, height {height}");
             }
         }
-
     }
 }
-
-//      public class Player
